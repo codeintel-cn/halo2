745c8f4495d972b025ed25ea85ba8735fd9cf31e
eric <ericinchina886@gmail.com>
2022-09-26 23:56:13 +0800

    update

---
+++ halo2_proofs/build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // use blstrs::{Fp, Fp2, G1Affine, G2Affine, Scalar};
+    use ec_gpu_gen::SourceBuilder;
+    use halo2curves::bn256::{Fq, Fq2, G1Affine, G2Affine, Fr};
+
+    let source_builder = SourceBuilder::new()
+        .add_fft::<Fr>()
+        .add_multiexp::<G1Affine, Fq>()
+        .add_multiexp::<G2Affine, Fq2>();
+    ec_gpu_gen::generate(&source_builder);
+}
